bbb3f18cb2df8d10cf1f187e60c5282fc0e6c66e
michmarmol <michmarmol94@gmail.com>
2019-06-13 20:54:22 +0200

    Device Unit Tests

--- Tests/DeviceTests.cs
+++ Tests/DeviceTests.cs
@@ -1,1 +1,17 @@
 //Testy jednostkowe sprawdzające poprawność działania metod klasy Device
+class DeviceTests{
+    Device testDevice= new Device();
+    SerialPort testPort=new SerialPort();
+public void WrongValue(int value)
+{
+    result = testDevice.GetValue(testPort);
+    
+    Assert.False(result, $"{value} is not a valid value");
+}
+public void CorrectValu(int value)
+{
+    result = testDevice.GetValue(testPort);
+
+    Assert.True(result, $"{value} is a correct value");
+}
+}
